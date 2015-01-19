1a29f16207678f6ea9ad54cef9997acc707e11af
roma <roman@r>
2015-01-19 22:34:09 +0200

    create project

---
+++ jsj/src/main/java/com/safetyjabber/App.java
@@ -0,0 +1,13 @@
+package com.safetyjabber;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
